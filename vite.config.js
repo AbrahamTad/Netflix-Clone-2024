588aa18547f233b70acdd5e0e0c9670bd997bd2d
Tad-Abraham <108827942+Eshkim@users.noreply.github.com>
2024-11-26 23:48:14 +0100

    update the base property

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default ({ mode }) => {
-  // Load env variables based on the current mode (e.g., development or production)
   const env = loadEnv(mode, process.cwd());
 
   return defineConfig({
-    base: "netflix-clone",
+    base: "/Netflix-Clone-2024/",
     plugins: [react()],
     define: {
       "process.env": env,
