9f039e4a4431da76b4fb8490cfcddfa35362eddf
Your Name <you@example.com>
2024-11-26 20:59:10 +0100

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default ({ mode }) => {
-  // Load env variables based on the current mode (e.g., development or production)
+  // Load env variables based on the current mode 
   const env = loadEnv(mode, process.cwd());
 
   return defineConfig({
